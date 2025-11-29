8565247a56ac82813fb0f46882392d5122608245
usaidyf <usaidsmart2005@gmail.com>
2025-11-29 16:31:56 +0500

    New lesson update (add logger.py)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
 from constants import (
-	SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_SPAWN_RATE,
+    ASTEROID_MAX_RADIUS,
 )
 from player import Player
 from asteroid import Asteroid
@@ -10,54 +15,54 @@ import sys
 
 
 def main():
-   pygame.init()
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   clock = pygame.time.Clock()
-   dt = 0
-   
-   asteroids = pygame.sprite.Group()
-   updatable = pygame.sprite.Group()
-   drawable = pygame.sprite.Group()
-   shots = pygame.sprite.Group()
-   
-   Player.containers = (updatable, drawable)
-   Asteroid.containers = (asteroids, updatable, drawable)
-   AsteroidField.containers = (updatable,)
-   Shot.containers = (shots, drawable, updatable)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-   player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-   asteroid_field = AsteroidField()
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-   score = 0
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, drawable, updatable)
 
-   while True:
-      for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-            print(f"Game exited. Your score: {score}")
-            return
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
-      screen.fill("black")
-      dt = clock.tick(60) / 1000
-      for thing in drawable:
-         thing.draw(screen)
-      for thing in updatable:
-         thing.update(dt)
-      for asteroid in asteroids:
-         if asteroid.check_collision(player):
-            print(f"Game over! Your score: {score}")
-            sys.exit(0)
-         for shot in shots:
-            if asteroid.check_collision(shot):
-               score += asteroid.split()
-               shot.kill()
-               print(f"Debug: score is now {score}")
+    score = 0
 
-      pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print(f"Game exited. Your score: {score}")
+                return
 
-   print("Starting Asteroids!")
-   print(f"Screen width: {SCREEN_WIDTH}")
-   print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        dt = clock.tick(60) / 1000
+        for thing in drawable:
+            thing.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print(f"Game over! Your score: {score}")
+                sys.exit(0)
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    score += asteroid.split()
+                    shot.kill()
+                    print(f"Debug: score is now {score}")
+
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
-   main()
+    main()
